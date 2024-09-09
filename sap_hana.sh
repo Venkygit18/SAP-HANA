6b9bfa740d3c7c392fff6f0bd240c946f42fb41d
vjanapati <45057953+Venkygit18@users.noreply.github.com>
2024-09-09 11:20:12 +0530

    Update sap_hana.sh

--- sap_hana.sh
+++ sap_hana.sh
@@ -22,7 +22,14 @@ backendpoolname="hana-db-BP"
 probename="hana-db-probe"
 sshpubkeyfile="/home/$USER/.ssh/id_rsa.pub"
 
+while getopts "i:" opt; do
+  case $opt in
+    i) offer=$OPTARG ;;
+    *) ;;
+  esac
+done
 
+echo "Offer: $offer"
 
 if [ -f "./username.txt" ]
 then
@@ -40,15 +47,50 @@ fi
 
 echo " "
 
+# Function to check if the app registration name exists
+check_app_exists() {
+  local appregname="$1"
+  app_exists=$(az ad app list --display-name "$appregname" --query "[0].appId" --output tsv)
+  [[ -n "$app_exists" ]]
+}
+
+# Prompt the user for the Azure fence agent device choice
 echo "Choose the Azure fence agent device:"
-echo "1. sp(service principal)"
-echo "2. msi(managed identity)"
+echo "1. sp (service principal)"
+echo "2. msi (managed identity)"
 read -p "Enter the number of your choice: " choice
 
+case $choice in
+  1)
+    # If the choice is 1, ask for the app registration name
+    while true; do
+      read -p "Enter the app registration name: " appregname
+
+      if check_app_exists "$appregname"; then
+        echo "An app registration with this name already exists. Please enter a different name."
+      else
+        echo "App registration name is available."
+        break
+      fi
+    done
+
+    # Output or use the appregname variable as needed
+    echo "The available app registration name is: $appregname"
+    ;;
+
+  2)
+    # If the choice is 2, just acknowledge the selection
+    echo "You selected managed identity (msi)."
+    ;;
+
+  *)
+    echo "Invalid choice. Please select either 1 or 2."
+    exit 1
+    ;;
+esac
+
 # Execute the corresponding function based on the user's choice
-
-echo ""
-
+echo " "
 date >> $logfile
 echo "Creating RG $rgname.."
 az group create --name $rgname --location $loc >> $logfile
@@ -164,19 +206,7 @@ if [ "$choice" = "1" ]; then
     subscriptionID=$(az group show --name "$rgname" --query "id" --output tsv | cut -d '/' -f 3) >> $logfile
     app_exists=$(az ad app list --display-name "$appregname" --query "[0].appId" --output tsv)
 
-    if [ -z "$app_exists" ]; then
-        echo "Azure AD App with the name '$appregname' doesn't exist."
-    else
-    # The App exists, so delete it
-        echo "Azure AD App with the name '$appregname' already exists. Deleting the existing App..."
-        keys=$(az ad app credential list --id $app_exists --query "[].keyId" --output tsv)
-        for key in $keys; do az ad app credential delete --id $app_exists --key-id $key; done
-        az ad app delete --id "$app_exists"
-        echo "Deleted existing Azure AD App with App ID: $app_exists"
-        sleep 60
-    fi
-
-    clientid=$(az ad app create --display-name $appregname --query appId --output tsv)
+   clientid=$(az ad app create --display-name $appregname --query appId --output tsv)
     #echo $clientid
     az ad sp create --id $clientid
     objectid=$(az ad app show --id $clientid --query objectId --output tsv)
