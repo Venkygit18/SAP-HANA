c9296ccb47d33af34da511e0a0d46e11c6e5f235
vjanapati <45057953+Venkygit18@users.noreply.github.com>
2025-07-30 10:43:41 +0530

    Add files via upload

---
+++ sapupd.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+
+logfile="./hana_cluster_log"
+#set -x
+
+rgname="sles-hana-rg-lab"
+loc="centralindia"
+asname="sleshana"
+vmname1="hn1-db-0"
+vmname2="hn1-db-1"
+vmname3="sbd-storage"
+lbname="sles-hana-lb"
+vnetname="hanavnet"
+subnetname="hanasubnet"
+sku_size="Standard_E4-2ads_v5"
+shared_disk=false
+offer="SUSE:sles-sap-15-sp5:gen1:latest"
+
+frontendip="hana-db-fwip"
+backendpoolname="hana-db-BP"
+probename="hana-db-probe"
+sshpubkeyfile="/home/$USER/.ssh/id_rsa.pub"
+
+while getopts "i:" opt; do
+  case $opt in
+    i) offer=$OPTARG ;;
+    *) ;;
+  esac
+done
+
+echo "Offer: $offer"
+
+if [ -f "./username.txt" ]
+then
+    username=`cat username.txt`
+else
+    read -p "Please enter the username: " username
+fi
+
+if [ -f "./password.txt" ]
+then
+    password=`cat password.txt`
+else
+    read -s -p "Please enter the password: " password
+fi
+
+echo " "
+
+# Execute the corresponding function based on the user's choice
+echo " "
+date >> $logfile
+echo "Creating RG $rgname.."
+az group create --name $rgname --location $loc >> $logfile
+
+echo "Async Creating availability set .."
+az vm availability-set create -n $asname -g $rgname --platform-fault-domain-count 2 --platform-update-domain-count 20  >> $logfile
+
+echo "Sync Creating VNET .."
+az network vnet create --name $vnetname -g $rgname --address-prefixes 10.0.0.0/24 --subnet-name $subnetname --subnet-prefixes 10.0.0.0/24  >> $logfile
+
+echo "Async Creating load balancer .."
+az network lb create --resource-group $rgname --name $lbname --location $loc --backend-pool-name $backendpoolname --frontend-ip-name $frontendip --private-ip-address "10.0.0.4" --sku "Standard" --vnet-name $vnetname --subnet $subnetname --no-wait >> $logfile
+
+echo "Async Creating First node, with both ssh key and password authentication methods .."
+az vm create -g $rgname -n $vmname1 --admin-username $username --admin-password $password --authentication-type "all"  --ssh-key-values $sshpubkeyfile --availability-set $asname --image $offer  --size "$sku_size" --vnet-name $vnetname --subnet $subnetname --public-ip-sku Standard --private-ip-address "10.0.0.5" --no-wait >> $logfile
+
+echo "Sync Creating Second node, with both ssh key and password authentication methods .."
+az vm create -g $rgname -n $vmname2 --admin-username $username --admin-password $password --authentication-type "all" --ssh-key-values $sshpubkeyfile --availability-set $asname --image $offer  --size "$sku_size" --vnet-name $vnetname --subnet $subnetname --public-ip-sku Standard --private-ip-address "10.0.0.6" >> $logfile
+
+echo "Connecting the machines to the load balancer .."
+az network lb probe create --lb-name $lbname --resource-group $rgname --name $probename --port 62503 --protocol Tcp >> $logfile
+
+nic1name1=`az vm show -g $rgname -n $vmname1  --query networkProfile.networkInterfaces[].id -o tsv | cut -d / -f 9`
+az network nic ip-config address-pool add --address-pool $backendpoolname --ip-config-name ipconfig$vmname1 --nic-name $nic1name1 --resource-group $rgname --lb-name $lbname  >> $logfile
+
+nic1name2=`az vm show -g $rgname -n $vmname2  --query networkProfile.networkInterfaces[].id -o tsv | cut -d / -f 9`
+az network nic ip-config address-pool add --address-pool $backendpoolname --ip-config-name ipconfig$vmname2 --nic-name $nic1name2 --resource-group $rgname --lb-name $lbname  >> $logfile
+
+echo "Creating load balancing rule .."
+az network lb rule create --resource-group $rgname --lb-name $lbname --name "Hana-DB-rule" --backend-port 0 --frontend-port 0 \
+ --frontend-ip-name $frontendip --backend-pool-name $backendpoolname --protocol All --floating-ip true \
+ --idle-timeout 30 --probe-name $probename  >> $logfile
+
+vm_1_pip=`az vm list-ip-addresses -g $rgname -n $vmname1 --query [0].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv`
+vm_2_pip=`az vm list-ip-addresses -g $rgname -n $vmname2 --query [0].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv`
+
+# getting VM 1 public key
+ssh -o "StrictHostKeyChecking=no" $username@$vm_1_pip 'sudo cat /dev/zero | sudo ssh-keygen -t rsa -q -N ""' >> $logfile
+vm_1_public_key=`ssh -o "StrictHostKeyChecking=no" $username@$vm_1_pip 'sudo cat /root/.ssh/id_rsa.pub'`
+
+#getting VM 2 public key
+ssh -o "StrictHostKeyChecking=no" $username@$vm_2_pip 'sudo cat /dev/zero | sudo ssh-keygen -t rsa -q -N ""' >> $logfile
+vm_2_public_key=`ssh -o "StrictHostKeyChecking=no" $username@$vm_2_pip 'sudo cat /root/.ssh/id_rsa.pub'`
+
+echo 'Done getting the ssh keys, updating both nodes to have passwordless root access between them'
+az vm run-command invoke -g $rgname -n $vmname1 --command-id RunShellScript --scripts "echo $vm_2_public_key > /root/.ssh/authorized_keys" >> $logfile
+az vm run-command invoke -g $rgname -n $vmname2 --command-id RunShellScript --scripts "echo $vm_1_public_key > /root/.ssh/authorized_keys" >> $logfile
+
+num_disks=4
+
+for ((i=1; i<=num_disks; i++)); do
+    disk_name="$vmname1-$i"
+    size_gb=30
+
+    az vm disk attach \
+        -g "$rgname" \
+        --vm-name "$vmname1" \
+        --name "$disk_name" \
+        --new \
+        --size-gb "$size_gb"
+
+    if [ $? -eq 0 ]; then
+        echo "Disk $disk_name successfully attached to $vmname1."
+    else
+        echo "Failed to attach disk $disk_name."
+    fi
+done
+
+num_disks=4
+
+for ((i=1; i<=num_disks; i++)); do
+    disk_name="$vmname2-$i"
+    size_gb=30
+
+    az vm disk attach \
+        -g "$rgname" \
+        --vm-name "$vmname2" \
+        --name "$disk_name" \
+        --new \
+        --size-gb "$size_gb"
+
+    if [ $? -eq 0 ]; then
+        echo "Disk $disk_name successfully attached to $vmname2."
+    else
+        echo "Failed to attach disk $disk_name."
+    fi
+done
+
+echo 'Entering the phase of configuring the cluster, we will start with node 2 then node 1'
+az vm extension set \
+--resource-group $rgname \
+--vm-name $vmname2 \
+--name customScript \
+--publisher Microsoft.Azure.Extensions \
+--protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/cluster_setup.sh"],"commandToExecute": "./cluster_setup.sh"}' >> $logfile
+
+az vm extension set \
+--resource-group $rgname \
+--vm-name $vmname1 \
+--name customScript \
+--publisher Microsoft.Azure.Extensions \
+--protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/cluster_setup.sh"],"commandToExecute": "./cluster_setup.sh"}' >> $logfile
+
+echo " configuring fencing device "
+
+clientid="0f0ca26c-1651-4123-a862-66d8016f34f7"
+clientsecretname=secretapp
+clientsecret="21ae57-b13a-4422-8206-0367e8e27db4fb"
+subscriptionID=$(az group show --name "$rgname" --query "id" --output tsv | cut -d '/' -f 3) >> $logfile
+az role assignment create --assignee $clientid --role "Virtual Machine Contributor" --scope  /subscriptions/$subscriptionID/resourceGroups/$rgname
+echo "creating fencing devices"
+
+export rgname
+export subscriptionID
+#export password
+
+wget -O susespn.sh https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/susespn.sh
+filename='susespn.sh'
+az vm run-command invoke -g $rgname -n $vmname1 --command-id RunShellScript --scripts @$filename --parameters $clientid $clientsecret $rgname $subscriptionID >>  $logfile
+
+echo "formatting data disks"
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname1 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings "{\"fileUris\": [\"https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/format.sh\"],\"commandToExecute\": \"./format.sh \"}" >> $logfile
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname2 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings "{\"fileUris\": [\"https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/format.sh\"],\"commandToExecute\": \"./format.sh \"}" >> $logfile
+
+echo "HANA installation node 1"
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname1 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/HAinstall.sh"],"commandToExecute": "./HAinstall.sh"}' >> $logfile
+
+echo "HANA installation node 2"
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname2 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/HAinstall.sh"],"commandToExecute": "./HAinstall.sh"}' >> $logfile
+
+
+echo " HA configure on node1"
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname1 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/hanaconfigure.sh"],"commandToExecute": "./hanaconfigure.sh"}' >> $logfile
+
+echo "hana sync primary node1"
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname1 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/hanasyncprim.sh"],"commandToExecute": "./hanasyncprim.sh"}' >> $logfile
+
+az vm run-command invoke --resource-group $rgname --name $vmname1 --command-id RunShellScript --scripts 'cd / && ./drsync.sh'
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname1 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/hn1primary.sh"],"commandToExecute": "./hn1primary.sh"}' >> $logfile
+
+echo "hana sync secondary node2 "
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname2 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/hanasyncsec.sh"],"commandToExecute": "./hanasyncsec.sh"}' >> $logfile
+
+az vm run-command invoke --resource-group $rgname --name $vmname2 --command-id RunShellScript --scripts 'cd / && ./drsync.sh'
+
+
+az vm extension set \
+    --resource-group $rgname \
+    --vm-name $vmname2 \
+    --name customScript \
+    --publisher Microsoft.Azure.Extensions \
+    --protected-settings '{"fileUris": ["https://raw.githubusercontent.com/Venkygit18/SAP-HANA/main/hn1sec.sh"],"commandToExecute": "./hn1sec.sh"}' >> $logfile
+
+az vm run-command invoke --resource-group $rgname --name $vmname2 --command-id RunShellScript --scripts 'cd / && ./drsync1.sh && sudo crm resource cleanup'
