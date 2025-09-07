3f8d469267c2839bbb9d5f35d3a2a2f73f211739
vjanapati <45057953+Venkygit18@users.noreply.github.com>
2025-09-07 23:48:41 +0530

    Update HAinstall.sh

--- HAinstall.sh
+++ HAinstall.sh
@@ -5,13 +5,13 @@ if [ ! -d "/etc/smbcredentials" ]; then
 sudo mkdir /etc/smbcredentials
 fi
 if [ ! -f "/etc/smbcredentials/saphanashare.cred" ]; then
-    sudo bash -c 'echo "username=saphanashare" >> /etc/smbcredentials/saphanashare.cred'
-    sudo bash -c 'echo "password=aNXBXiie63MA2zg5ZXAMD7QyTgLMpqFc3d15/XeJCwWz41oF3+Zi9oo6/JZE/fY4Ro2slpJv+erw+AStJqNgMA==" >> /etc/smbcredentials/saphanashare.cred'
+    sudo bash -c 'echo "username=saphanashare1" >> /etc/smbcredentials/saphanashare.cred'
+    sudo bash -c 'echo "password=OveDL1gVIPzChI/kQHtAYVABistIjo0nMYXuyhiyznRDL/CjomR4ub4XcrxEM5Br5w7P35veqprG+ASt1WgD5Q==" >> /etc/smbcredentials/saphanashare.cred'
 fi
 sudo chmod 600 /etc/smbcredentials/saphanashare.cred
 
-sudo bash -c 'echo "//saphanashare.file.core.windows.net/hanashare /mnt/hanashare cifs nofail,credentials=/etc/smbcredentials/saphanashare.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab'
-sudo mount -t cifs //saphanashare.file.core.windows.net/hanashare /mnt/hanashare -o credentials=/etc/smbcredentials/saphanashare.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
+sudo bash -c 'echo "//saphanashare1.file.core.windows.net/hanashare /mnt/hanashare cifs nofail,credentials=/etc/smbcredentials/saphanashare.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30" >> /etc/fstab'
+sudo mount -t cifs //saphanashare1.file.core.windows.net/hanashare /mnt/hanashare -o credentials=/etc/smbcredentials/saphanashare.cred,dir_mode=0777,file_mode=0777,serverino,nosharesock,actimeo=30
 
 scp -r /mnt/hanashare/HDB_SERVER_LINUX_X86_64 /
 
